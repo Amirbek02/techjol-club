b4119c6ef73e3b8256bfc24c74d266275bebd9bc
Amirbek02 <amirbek.amiraev.kk@gmail.com>
2024-10-29 16:55:46 +0600

    dsf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Partners from '@/components/shared/partners';
 import { SignInPage } from '@/components/shared/signInPage';
+import { SignUpPage } from '@/components/shared/signUpPage';
 
 export default function Home() {
   return (
     <div>
       <Partners />
       <SignInPage />
+      <SignUpPage />
     </div>
   );
 }
