0d1eb8d310313bd828cb6b559046cca1cd2ccc30
Bekbolot <burxanovbeko@gmail.com>
2024-11-11 20:48:35 +0600

    carousel pikirler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import TestimonialCarousel from "@/components/shared/carouseler";
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <TestimonialCarousel />
+    </div>
+  );
 }
