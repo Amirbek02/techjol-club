8c503b9f953b7b0ca0a6a7a67aefa9d5474af88c
Bekbolot <burxanovbeko@gmail.com>
2024-11-13 22:11:13 +0600

    f

---
+++ src/components/shared/footeradmin.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface Props {
+  className?: string;
+}
+
+export const FooterAdmine: React.FC<Props> = ({ className }) => {
+  return (
+    <div>
+      <div
+        style={{
+          backgroundColor: "#031634",
+        }}
+        className="p-5 flex items-center justify-center text-amber-400"
+      >
+        <a href="">Copyright © 2024</a>
+      </div>
+    </div>
+  );
+};
