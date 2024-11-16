3b14962faf9e1d40ae262417f3fe15573aab0c15
Bekbolot <burxanovbeko@gmail.com>
2024-11-16 17:12:21 +0600

    2

--- src/components/shared/sidebar.tsx
+++ src/components/shared/sidebar.tsx
@@ -1,12 +1,14 @@
 "use client";
+import Link from "next/link";
 import React from "react";
+
 interface Props {
   className?: string;
 }
 
 export const Sidebar: React.FC<Props> = ({ className }) => {
   return (
-    <div>
+    <div className={className}>
       <div
         style={{
           backgroundColor: "#031634",
@@ -14,45 +16,45 @@ export const Sidebar: React.FC<Props> = ({ className }) => {
         className="w-[395px] h-screen fixed top-0 left-0  text-white "
       >
         <ul className=" pt-10 space-y-8">
-          <li className=" flex items-center justify-evenly font-semibold text-2xl">
-            <a className="hover:text-yellow-500" href="">
+          <li className=" flex items-center justify-start pl-32 font-semibold text-xl">
+            <Link className="hover:text-yellow-500" href="">
               Башкы бет
-            </a>
+            </Link>
           </li>
-          <li className=" flex items-center justify-evenly font-semibold text-2xl">
-            <a className="hover:text-yellow-500" href="">
+          <li className=" flex items-center justify-start pl-32  font-semibold text-xl">
+            <Link className="hover:text-yellow-500" href="">
               Багыттар
-            </a>
+            </Link>
           </li>
-          <li className=" flex items-center justify-center font-semibold text-2xl ">
-            <a className="hover:text-yellow-500" href="">
+          <li className=" flex items-center justify-start pl-32 font-semibold text-xl ">
+            <Link className="hover:text-yellow-500" href="">
               Өнөктөштөр
-            </a>
+            </Link>
           </li>
-          <li className=" flex items-center justify-center font-semibold text-2xl">
-            <a className="hover:text-yellow-500" href="">
+          <li className=" flex items-center justify-start pl-32 font-semibold text-xl">
+            <Link className="hover:text-yellow-500" href="">
               Менторлор
-            </a>
+            </Link>
           </li>
-          <li className=" flex items-center justify-evenly font-semibold text-2xl">
-            <a className="hover:text-yellow-500" href="">
+          <li className=" flex items-center justify-start pl-32 font-semibold text-xl">
+            <Link className="hover:text-yellow-500" href="">
               Пикирлер
-            </a>
+            </Link>
           </li>
-          <li className=" flex items-center justify-evenly font-semibold text-2xl">
-            <a className="hover:text-yellow-500" href="">
+          <li className=" flex items-center justify-start pl-32 font-semibold text-xl">
+            <Link className="hover:text-yellow-500" href="">
               Блогдор
-            </a>
+            </Link>
           </li>
-          <li className=" flex items-center justify-center font-semibold text-2xl ">
-            <a className="hover:text-yellow-500" href="">
+          <li className=" flex items-center justify-start pl-32 font-semibold text-xl ">
+            <Link className="hover:text-yellow-500" href="">
               Катышуучулар
-            </a>
+            </Link>
           </li>
-          <li className=" flex items-center justify-center font-semibold  text-2xl">
-            <a className="hover:text-yellow-500" href="">
+          <li className=" flex items-center justify-start pl-32  font-semibold  text-xl">
+            <Link className="hover:text-yellow-500 pl-30" href="">
               Кулактандыруу
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
